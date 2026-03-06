c23ffc04f041cd09051d31c829c23137314c6068
encarbassot <elgrefa@gmail.com>
2026-03-06 22:36:09 +0100

    fisrt commit

---
+++ api/index.js
@@ -0,0 +1,8 @@
+import * as Elio from 'elio-express-bundle'
+
+console.log('Elio exports:', Object.keys(Elio))
+
+// Example: use exports from the linked package
+const api = Elio
+
+export default api
